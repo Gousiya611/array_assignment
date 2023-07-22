3ff8fad35a924de9c4ce6c440173ef32a4c9f628
Shaik Gousiya <gousiya611shaik@gmail.com>
2023-07-22 22:11:57 +0530

    ch

---
+++ assignment/missingElement.java
@@ -0,0 +1,54 @@
+package assignment;
+import java.util.Scanner;
+// import java.util.*;
+
+public class missingElement {
+    public static void main(String[] args) {
+    System.out.println("Enter the size of array:");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter the array elements:");
+
+    for(int i=0; i<n; i++){
+        arr[i]=sc.nextInt();
+
+    }
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for(int nu: arr){
+        if(nu<min){
+            min =nu;
+        }
+        if(nu>max){
+            max= nu;
+        }
+    }
+
+    boolean[] presentElement = new boolean[max-min+1];
+    for(int num: arr){
+        presentElement[num - min] = true;
+    }
+
+    System.out.println("Missing elements: ");
+    for(int j=0; j<presentElement.length ; j++){
+        if(!presentElement[j]){
+            System.out.println(j+min+" ");
+        }
+    }
+    sc.close();
+} 
+}
+
+
+    // public static void main(String[] args){
+    // Scanner sc = new Scanner(System.in);
+    // int n = sc.nextInt();
+    // int[] arr = new int[n];
+
+    // for(int i=0; i<n; i++){
+    //     arr[i]=sc.nextInt();
+
+    // }
+   
+    
